1f2b811e2bc70350dc5d25687e8f8e9d2d556be2
Sokol Misha <sokol.misha.m.com>
2022-09-20 17:12:25 +0300

    Add Template class which returns html template for note

---
+++ index.js
@@ -0,0 +1,7 @@
+import './src/css/app.css';
+
+import App from './src/js/app';
+
+const app = new App();
+
+app.init();
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+class App {
+  init() {
+    console.log('init');
+  }
+}
+
+export default App;
